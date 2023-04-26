6ed08fb98d44aadf6e9b5bd1e517318ddf5dc9d9
Adela3003 <79999674+Adela3003@users.noreply.github.com>
2023-04-26 16:04:45 +0300

    autentificare shared preferences

---
+++ app/src/main/java/com/example/amg_tennis_club_android/HomeActivity.java
@@ -0,0 +1,57 @@
+package com.example.amg_tennis_club_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView textView_name, textView_pass;
+    Button button_logout;
+
+    SharedPreferences sharedPreferences;
+
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PASS = "password";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        textView_name = findViewById(R.id.textFullName);
+        textView_pass = findViewById(R.id.textPassword);
+        button_logout = findViewById(R.id.buttonLogOut);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        String name = sharedPreferences.getString(KEY_NAME, null);
+        String password = sharedPreferences.getString(KEY_PASS, null);
+
+        if(name != null || password != null) {
+            textView_name.setText("Welcome "+ name);
+        }
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                finish();
+                Toast.makeText(HomeActivity.this, "LOGOUT SUCCESSFULLY", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
+}
